5be705865a4c6ca596db079136176a6568a3d6cf
Elgin <elgin_mclaren@hotmail.com>
2017-12-09 21:09:23 -0800

    am able to manipulate the dom

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../project';
 import { ProjectService } from '../project.service';
-// import * as Shuffle from 'shufflejs';
- 
+import * as Shuffle from 'shufflejs';
+import {AfterContentInit, ContentChildren, Directive, QueryList, Renderer } from '@angular/core';
+import {  ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.scss' ]
+})
+
+
+
+
+  export class AppComponent implements AfterViewInit {
+    @ViewChild('myId') myId: ElementRef;
+
+  
+    constructor() {
+    }
+
+    ngAfterViewInit() {
+      console.log(this.myId.nativeElement);
+      console.log("hi")
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+// @Directive({ 
+//   selector: 'li' 
+// })
+// export class ListItem {}
+
+// @Component({
+//   selector: 'my-list'
+// })
+
+
+// export class MyList implements AfterContentInit {
+  
+//   @ContentChildren(ListItem) items: QueryList<ListItem>;
+//   ListItem.shouldShow = false;
+
+//   ngAfterContentInit() {
+//  // this.renderer.invokeElementMethod(this.input.nativeElement,    
+//  //    'focus');
+//   }
+// }
 
  
 @Component({
@@ -10,82 +63,124 @@ import { ProjectService } from '../project.service';
   templateUrl: './dashboard.component.html',
   styleUrls: [ './dashboard.component.scss' ]
 })
+
 export class DashboardComponent implements OnInit {
   projects: Project[] = [];
  
+
+
+
+@ViewChild('myId') myId: ElementRef;
+
+  
+   
   constructor(private projectService: ProjectService) { }
  
   ngOnInit() {
     this.getProjects();
+      console.log(this.myId.nativeElement);
+      console.log("hi")
+    
+
+const shuffleInstance = new Shuffle(this.myId.nativeElement) {
+  // itemSelector: '.js-item',
+  // sizer: '.js-shuffle-sizer'
+});
+
+   
   }
  
   getProjects(): void {
     this.projectService.getProjects()
       .subscribe(projects => this.projects = projects.slice(0, 5));
+      
   }
 
-
-
-
-angular
-  .module('MyApp',['ngMaterial', 'ngMessages'])
-  .controller('gridListDemoCtrl', function($scope) {
-
-    this.tiles = buildGridModel({
-            icon : "avatar:svg-",
-            title: "Svg-",
-            background: ""
-          });
-
-    function buildGridModel(tileTmpl){
-      var it, results = [ ];
-
-      for (var j=0; j<11; j++) {
-
-        it = angular.extend({},tileTmpl);
-        it.icon  = it.icon + (j+1);
-        it.title = it.title + (j+1);
-        it.span  = { row : 1, col : 1 };
-
-        switch(j+1) {
-          case 1:
-            it.background = "red";
-            it.span.row = it.span.col = 2;
-            break;
-
-          case 2: it.background = "green";         break;
-          case 3: it.background = "darkBlue";      break;
-          case 4:
-            it.background = "blue";
-            it.span.col = 2;
-            break;
-
-          case 5:
-            it.background = "yellow";
-            it.span.row = it.span.col = 2;
-            break;
-
-          case 6: it.background = "pink";          break;
-          case 7: it.background = "darkBlue";      break;
-          case 8: it.background = "purple";        break;
-          case 9: it.background = "deepBlue";      break;
-          case 10: it.background = "lightPurple";  break;
-          case 11: it.background = "yellow";       break;
-        }
-
-        results.push(it);
-      }
-      return results;
-    }
-  })
-  .config( function( $mdIconProvider ){
-    $mdIconProvider.iconSet("avatar", 'icons/avatar-icons.svg', 128);
-  });
-
-
 }
-// const shuffleInstance = new Shuffle(document.getElementsByClassName('grid')[0],{
-//   itemSelector: '.js-item',
-//   sizer: '.js-item'
-// });
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { Project } from '../project';
+// import { ProjectService } from '../project.service';
+// // import * as Shuffle from 'shufflejs';
+ 
+// declare var p5: any;
+
+
+ 
+// @Component({
+//   selector: 'app-dashboard',
+//   templateUrl: './dashboard.component.html',
+//   styleUrls: [ './dashboard.component.scss' ]
+// })
+
+// export class DashboardComponent implements OnInit {
+//   projects: Project[] = [];
+ 
+
+
+//  play: boolean = false;
+
+
+
+//   constructor(private projectService: ProjectService) { }
+ 
+//   ngOnInit() {
+//     this.getProjects();
+
+
+//     const s = (p) => {
+
+//       let song;
+//       let canvas;
+
+//       p.preload = () => {
+//         console.log('preload');
+//         // song = p.loadSound('assets/music/Thunderstruck.mp3');
+//       }
+
+//       p.setup = () => {
+//         canvas = p.createCanvas(595, 100);
+//         canvas.parent('equalizer');
+//         p.background(0);
+//       }
+//     }
+
+//     let player = new p5(s);
+
+//   }
+ 
+//   getProjects(): void {
+//     this.projectService.getProjects()
+//       .subscribe(projects => this.projects = projects.slice(0, 5));
+      
+//   }
+
+
+
+//   onPlay() {
+//     this.play = !this.play;
+
+//   }
+
+// }
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+  
 
