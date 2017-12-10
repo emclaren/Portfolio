2685d2d1414ce096fa32d42c07357ec907b4508c
Elgin <elgin_mclaren@hotmail.com>
2017-12-10 11:36:44 -0800

    fixed syntax error in code

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,59 +5,8 @@ import * as Shuffle from 'shufflejs';
 import {AfterContentInit, ContentChildren, Directive, QueryList, Renderer } from '@angular/core';
 import {  ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
-@Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.scss' ]
-})
 
 
-
-
-  export class AppComponent implements AfterViewInit {
-    @ViewChild('myId') myId: ElementRef;
-
-  
-    constructor() {
-    }
-
-    ngAfterViewInit() {
-      console.log(this.myId.nativeElement);
-      console.log("hi")
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-// @Directive({ 
-//   selector: 'li' 
-// })
-// export class ListItem {}
-
-// @Component({
-//   selector: 'my-list'
-// })
-
-
-// export class MyList implements AfterContentInit {
-  
-//   @ContentChildren(ListItem) items: QueryList<ListItem>;
-//   ListItem.shouldShow = false;
-
-//   ngAfterContentInit() {
-//  // this.renderer.invokeElementMethod(this.input.nativeElement,    
-//  //    'focus');
-//   }
-// }
-
- 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -66,35 +15,30 @@ import {  ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 export class DashboardComponent implements OnInit {
   projects: Project[] = [];
- 
-
-
-
-@ViewChild('myId') myId: ElementRef;
-
+  @ViewChild('myId') myId: ElementRef;
   
-   
+
   constructor(private projectService: ProjectService) { }
- 
+
   ngOnInit() {
     this.getProjects();
-      console.log(this.myId.nativeElement);
-      console.log("hi")
-    
-
+    console.log(this.myId.nativeElement);
+    console.log("hi")
   }
 
-   ngAfterViewInit() {
-const shuffleInstance = new Shuffle(this.myId.nativeElement) {
-  // itemSelector: '.js-item',
-  // sizer: '.js-shuffle-sizer'
+  ngAfterViewInit() {
+
+
+
+    const shuffleInstance = new Shuffle(this.myId.nativeElement, {
+  
 });
+
   }
- 
+
   getProjects(): void {
     this.projectService.getProjects()
-      .subscribe(projects => this.projects = projects.slice(0, 5));
-      
+    .subscribe(projects => this.projects = projects.slice(0, 5));
   }
 
 }
@@ -104,78 +48,6 @@ const shuffleInstance = new Shuffle(this.myId.nativeElement) {
 
 
 
-// import { Component, OnInit } from '@angular/core';
-// import { Project } from '../project';
-// import { ProjectService } from '../project.service';
-// // import * as Shuffle from 'shufflejs';
- 
-// declare var p5: any;
-
-
- 
-// @Component({
-//   selector: 'app-dashboard',
-//   templateUrl: './dashboard.component.html',
-//   styleUrls: [ './dashboard.component.scss' ]
-// })
-
-// export class DashboardComponent implements OnInit {
-//   projects: Project[] = [];
- 
-
-
-//  play: boolean = false;
-
-
-
-//   constructor(private projectService: ProjectService) { }
- 
-//   ngOnInit() {
-//     this.getProjects();
-
-
-//     const s = (p) => {
-
-//       let song;
-//       let canvas;
-
-//       p.preload = () => {
-//         console.log('preload');
-//         // song = p.loadSound('assets/music/Thunderstruck.mp3');
-//       }
-
-//       p.setup = () => {
-//         canvas = p.createCanvas(595, 100);
-//         canvas.parent('equalizer');
-//         p.background(0);
-//       }
-//     }
-
-//     let player = new p5(s);
-
-//   }
- 
-//   getProjects(): void {
-//     this.projectService.getProjects()
-//       .subscribe(projects => this.projects = projects.slice(0, 5));
-      
-//   }
-
-
-
-//   onPlay() {
-//     this.play = !this.play;
-
-//   }
-
-// }
-
-
-
-
-
-
-  
 
 
 
@@ -183,5 +55,11 @@ const shuffleInstance = new Shuffle(this.myId.nativeElement) {
 
 
 
-  
+
+
+
+
+
+
+
 
