bcd8ce39bbdad073708cc11f9efb34ac50a3f98b
Elgin <elgin_mclaren@hotmail.com>
2017-12-15 09:16:55 -0800

    fix mobile appearance

--- src/app/my-projects.ts
+++ src/app/my-projects.ts
@@ -6,72 +6,111 @@ export const PROJECTS: Project[] = [
 	name: 'Mind-Full',
   thumbname: 'Neurofeedback',
   class: 'mind-full',
-	summary:'In 2016 I joined a team working to improve educational outcomes for kids with anxiety and attentional challenges.',
+   thumbnail:'/assets/img/mind-full-thumbnail-min.jpg'
+	summary:'A neurofeedback application to improve educational outcomes for children with anxiety and attentional challenges.',
 	overview: 'Mind-Full is an EEG-neurofeedback system to help kids ages 6-10 learn to self-regulate.',
 	team: 'Alissa N. Antle, Perry Tan, Srilekha Kirsh Sridharan, Fan Lin, Emily Cramer',
-	tags: 'NeuroSky EEG Headset, Unity Game Engine, Android Application Development, Sound',
+	tags: 'NeuroSky EEG Headset, Unity Game Engine, Android Development, Audio',
 	role: 'Developer, Tester, Project Manager, Researcher (further details below)',
-  thumbnail:'/assets/img/mind-full-thumbnail-min.jpg'
+  challenge: 'Children in North America are more likely to suffer from anxiety and attentional challenges than any other mental health issues. Mind-Full was developed to help these children learn to self-regulate. Using commercial EEG-headset technolgoy, Mind-Full provides real-time feedback on children\'s stress and attention levels and rewards them through game animations for achieving customizable thresholds. Results from our research studies suggest kids can take these skills and apply them outside of context of using the app.',
+  process: 'testing process',
+  result: '',
+  image1: '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/2TbLI6mga38" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe> ',
+  image2: '',
+  image3: ''
+ 
 },
 { id: 2, 
 	name: 'Block Talks', 
   thumbname: 'A.R. for Literacy',
   class: 'block-talks',
-	summary:'In 2016 I joined a team working to improve educational outcomes for kids with anxiety and attentional challenges.',
-	overview: 'Block Talks is an EEG-neurofeedback system to help kids ages 6-10 learn to self-regulate.',
-  team: 'Block talksrry Tan, Srilekha Kirsh Sridharan, Fan Lin, Emily Cramer',
-  tags: 'block talks, Unity Game Engine, Android Application Development, Sound',
-  role: 'Dblocktalkser, Tester, Project Manager, Researcher (further details below)',  
   thumbnail:'/assets/img/block-talks-thumbnail-min.jpg'
+	summary:'In fall 2017 a team of students in my lab won 3rd place (out of 66 teams) in Eduhacks Hackathon, for our app designed to improve children\'s literacy.',
+	overview: 'Block talks is a tangible Augmented Reality toolkit to help kids ages 7-9 learn to read.',
+  team: 'Uddipana Baishya, Min Fan, Shubhra Sarkar, and Amal Vincent',
+  tags: 'Augmented Reality, Unity Game Engine, Tangible, Games for Education',
+  role: 'Ideation, Development, Project Pitch (details below)', 
+  challenge: '',
+  process: '',
+  result: '',
+  image1: '',
+  image2: '',
+  image3: '' 
+  
 },
 { id: 3, 
   
   name: 'Audio-Matic',
   thumbname: 'A.I. Music',
   class: 'audio-matic',
-  summary:'In 2016 I joined a team working to improve educational outcomes for kids with anxiety and attentional challenges.',
-  overview: 'XXX about audiomatc',
+   thumbnail:'/assets/img/audio-matic-thumbnail-min.jpg'
+  summary:'In winter 2017, I created an interactive audio applications for AI toolkits',
+  overview: 'Audio-Matic is a novel means of encouraging playful, accessible experiences with AI technology.',
   team: 'Contributions by Dr. Steve Dipaolo',
-  tags: 'Audiomaticset, Unity Game Engine, Android Application Development, Sound',
-  role: 'Developer, Tester, Project Manager, Researcher (further details below)',
+  tags: 'Deep Learning (Magenta/TensorFlow), MaxMSP, Arduino Microcontroller, Hardware, Sound',
+  role: 'Designer, Developer',
   challenge: 'Challenge',
   process: 'next steps',
-  
-  thumbnail:'/assets/img/audio-matic-thumbnail-min.jpg'
+  challenge: '',
+  process: '',
+  result: '',
+  image1: '',
+  image2: '',
+  image3: ''
+ 
 },
 { id: 4, 
   name: 'Tangible Audio', 
    thumbname: 'Tangible Audio',
    class: 'tangibles',
-  	summary:'In 2016 I joined a team working to improve educational outcomes for kids with anxiety and attentional challenges.',
-  	overview: 'Mind-Full is an EEG-neurofeedback system to help kids ages 6-10 learn to self-regulate.',
-  	team: 'Alissa N. Antle, Perry Tan, Srilekha Kirsh Sridharan, Fan Lin, Emily Cramer',
-  	tags: 'NeuroSky EEG Headset, Unity Game Engine, Android Application Development, Sound',
-  	role: 'Developer, Tester, Project Manager, Researcher (further details below)',
-      
-  thumbnail:'/assets/img/tangibles-thumbnail-min.jpg'
+     thumbnail:'/assets/img/tangibles-thumbnail-min.jpg'
+  	summary:'Beginning in Fall 2015, I began developing tech to lowering the barriers to kids musical performance',
+  	overview: 'Tangible instruments is a series of DIY music controllers to help kids with performance anxiety feel comfortable playing music.',
+  	team: 'Contributions from campers at Rock Camp for Girls Montreal & New York',
+  	tags: 'Open source Microcontrollers, NodeJS, Sensors, Toys and Tangibles',
+  	role: 'Designer, Developer, UX researcher',
+      challenge: '',
+  process: '',
+  result: '',
+  image1: '',
+  image2: '',
+  image3: ''
+
 },
 { id: 5, 
   name: 'Virtual Earthgazing',
    thumbname: 'V.R. Earthgazing',
    class: 'earthgazing',
-  	summary:'In 2016 I joined a team working to improve educational outcomes for kids with anxiety and attentional challenges.',
-  	overview: 'Mind-Full is an EEG-neurofeedback system to help kids ages 6-10 learn to self-regulate.',
-  	team: 'Alissa N. Antle, Perry Tan, Srilekha Kirsh Sridharan, Fan Lin, Emily Cramer',
-  	tags: 'NeuroSky EEG Headset, Unity Game Engine, Android Application Development, Sound',
-  	role: 'Developer, Tester, Project Manager, Researcher (further details below)',
-  thumbnail:'/assets/img/earthgazing-thumbnail.jpg'
+     thumbnail:'/assets/img/earthgazing-thumbnail.jpg'
+  	summary:'In fall 2017 I collaborated with researcher from the iSpace lab to study the power of VR to make people feel more connected to the earth.',
+  	overview: 'Virtual Earthgazing is a project investigating the potential of VR to enable individuals to experience the overview effect, the awe-inspiring experience of seeing Earth from space resulting in a heightened awareness of the planet.',
+  	team: 'Worked with with SIAT\'s iSpace lab ( Bernard Riecke, Katerina Stepanova, Denise Quesnel, Alex Kitson, Mirjana Prpa, Ivan Aguilar',
+  	tags: 'Virtual Reality, HTC Vive, Unity Game Engine',
+  	role: 'Researcher (details below)',
+    challenge: '',
+  process: '',
+  result: '',
+  image1: '',
+  image2: '',
+  image3: ''
+
  },
 { id: 6, name: 'Asking for a Friend',
- class: 'asking-for-a-friend', 
   thumbname: 'Health Chatbot',
-  	summary:'In 2016 I joined a team working to improve educational outcomes for kids with anxiety and attentional challenges.',
-  	overview: 'Mind-Full is an EEG-neurofeedback system to help kids ages 6-10 learn to self-regulate.',
-  	team: 'Alissa N. Antle, Perry Tan, Srilekha Kirsh Sridharan, Fan Lin, Emily Cramer',
-  	tags: 'NeuroSky EEG Headset, Unity Game Engine, Android Application Development, Sound',
-  	role: 'Developer, Tester, Project Manager, Researcher (further details below)',
-    
-  thumbnail:'assets/img/asking-for-a-friend-thumbnail-min.jpg'
+  class: 'asking-for-a-friend', 
+   thumbnail:'assets/img/asking-for-a-friend-thumbnail-min.jpg'
+  	summary:'In fall 2017, a team of students developed a tool to improving accessibility of mental health resources for youth.',
+  	overview: 'Asking For a Friend is a chatbot designed to connect individuals seeking help with mental health challenges with local resources and support.',
+  	team: 'Vivian Pan, Joanna Zhao, Natasha Caton',
+  	tags: 'NodeJS, Microsoft Azure Bot Framework',
+  	role: 'DeIdeation, Developed front end usability, Coded the backend with Vivian Pan',
+    challenge: '',
+  process: '',
+  result: '',
+  image1: '',
+  image2: '',
+  image3: ''
+ 
 
 }
 
