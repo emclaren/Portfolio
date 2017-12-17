43a7bcb0f6035fe6598439ddd29a83e5651adbb4
Elgin <elgin_mclaren@hotmail.com>
2017-12-17 11:42:19 -0800

    commit before starting to test scrolling-to-the-top  issue

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -36,8 +36,10 @@ export class ProjectDetailComponent implements OnInit {
  this.router.events.subscribe((evt) => {
         if (!(evt instanceof NavigationEnd)) {
             return;
+            console.log("bug test 1");
         }
-        // document.body.scrollTop = 0;
+        console.log("bug test 2");
+        document.body.scrollTop = 0;
         window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
     });
 
@@ -66,4 +68,65 @@ export class ProjectDetailComponent implements OnInit {
 
 
 
+
+
+// import { Component, OnInit, Input } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { Location } from '@angular/common';
+
+// import { Project }         from '../project';
+// import { ProjectService }  from '../project.service';
+// import { Router, NavigationEnd } from '@angular/router';
+
+// @Component({
+//   selector: 'app-project-detail',
+//   templateUrl: './project-detail.component.html',
+//   styleUrls: [ './project-detail.component.scss' ]
+// })
+// export class ProjectDetailComponent implements OnInit {
+//   @Input() project: Project;
+ 
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private projectService: ProjectService,
+//     private location: Location,
+//     private router: Router
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.getProject();
+ 
+
+
+//  this.router.events.subscribe((evt) => {
+//         if (!(evt instanceof NavigationEnd)) {
+//             return;
+//         }
+//         // document.body.scrollTop = 0;
+//         window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+//     });
+
+
+
+//   }
+
+//   getProject(): void {
+//     const id = +this.route.snapshot.paramMap.get('id');
+//     this.projectService.getProject(id)
+//       .subscribe(project => this.project = project);
+//   }
+
+//   goBack(): void {
+//     this.location.back();
+//   }
+
+
+
+// }
+
+
+// ]
+
+   
    
