e3a17585a32ea10900acee0814bc038b3a0f7bbc
Elgin <elgin_mclaren@hotmail.com>
2018-01-14 09:12:36 -0800

    fix mobile image sizing

--- src/app/project.ts
+++ src/app/project.ts
@@ -3,49 +3,39 @@ export class Project {
   name: string;
   thumbname: string;
   class: string;
-   thumbnail:string;
-	summary:string;
-	overview:string;
-	team: string;
-	tags: string;
-	role: string;
-   challengeHeading:string;
-    c1:string;
+  thumbnail:string;
+  summary:string;
+  overview:string;
+  team: string;
+  tags: string;
+  role: string;
+
+  challengeHeading:string;
+  c1:string;
   c2:string;
   c3:string;
   c4:string;
+
   problemHeading1:string;
   problemHeading2:string;
-
   p1:string;
   p2:string;
   p3:string;
   p4:string;
   p5: string;
+
   resultHeading1:string;
   resultHeading2: string;
-  
   r1:string;
   r2:string;
-r3: string;
-   rImage1:string;
-  rImage2:string;
-  rImage3:string;
-  rImage1Class:string;
-  rImage2Class:string;
-  rImage3Class:string;
-  rImage4:string;
-  rImage5:string;
-  rImage6:string;
-  rImage4Class:string;
-  rImage5Class:string;
-  rImage6Class:string;
+  r3: string;
+
   caption1:string;
   caption2:string;
   caption3:string;
   caption4:string;
   caption5:string;
   caption6:string;
-    caption7:string;
- 
+  caption7:string;
+
 }
